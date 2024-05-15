123f23534005638274b876f65b8d7e129d6e51ce
LaurenR97 <156872197+LaurenR97@users.noreply.github.com>
2024-05-14 23:31:57 -0400

    Add files via upload

---
+++ schema table/DeathRate.sql
@@ -0,0 +1,39 @@
+DROP TABLE IF EXISTS  country;
+DROP TABLE IF EXISTS cause_of_death;
+
+
+CREATE TABLE country (
+    country_id int PRIMARY KEY NOT NULL,
+    Country varchar(50),
+    Country_code varchar(50)
+);
+
+CREATE TABLE "cause_of_death" (
+    "country_id" int NOT NULL,
+    "year" int NOT NULL,
+    "High_Blood_Pressure" int NOT NULL,
+    "Alcohol" int NOT NULL,
+    "Toxic_Water" int NOT NULL,
+    "Secondhand_smoke" int NOT NULL,
+	"Low_birth_weight" int NOT NULL,
+    "Child_malnutrition" int NOT NULL,
+	"Unsafe_sex" int NOT NULL,
+	"Household_air_pollution" int NOT NULL,
+    "Sedentary_lifestyle" int NOT NULL,
+    "Smoking" int NOT NULL,
+    "Hyperglycemia" int NOT NULL,
+	"High_BMI" int NOT NULL,
+    "Unsafe_sanitation" int NOT NULL,
+    "Lack_of_handwashing_facility" int NOT NULL,
+    "Drugs" int NOT NULL,
+    "Osteoporosis" int NOT NULL,
+	"Stunting_of_growth" int NOT NULL,
+    "Air_Pollution" int NOT NULL,
+    "Poor_Nutrition" int NOT NULL,
+    "Partial_breastfeeding" int NOT NULL,
+	"Elements_Deficiency" int NOT NULL,
+    FOREIGN KEY (country_id) references country(country_id)
+);
+
+Select * from cause_of_death
+Select * from country
